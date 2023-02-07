b3c4e607916376ea9c046e72d251a6369dd48765
TheCozbrozYT <81248026+TheCozbrozYT@users.noreply.github.com>
2023-02-06 19:58:53 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         <code>Bubaid Nhatti</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://ubweb.cozbroz.repl.co"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          UBWEB
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
